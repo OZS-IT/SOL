cfb136969d1a54c133158ff3e811e2541cc0c50f
andrejborstnik <andrej-borstnik@hotmail.com>
2014-04-10 08:35:51 +0200

    Popravljeno.

--- pretvori_registracije.py
+++ pretvori_registracije.py
@@ -1,18 +1,21 @@
 from urllib.request import urlopen
 
-with open("Registracije/registracije2.csv","r",encoding="utf-8") as f:
-    with open("Registracije/registracije2a.csv","w",encoding="utf-8") as g:
+with open("Registracije/registracije1.csv","r",encoding="utf-8") as f:
+    with open("Registracije/registracije1c.csv","w",encoding="utf-8") as g:
         a = f.read()
         b = a.split("\n")
         ozs = urlopen("http://www.orientacijska-zveza.si/index.php?id=56")
         stran=str(ozs.read(),encoding="utf-8")
         s = ""
+        tujci = ["Herwig Allwinger jr.", "Andraž De Luisa", "Vedran Bijelič",
+                 "Manuel Jurado", "Marko Dudić"]
         for i in b:
             stri=""
             c = i.split(";")
             k = 0
             if len(c)>1:
-                if not (c.join(" ")+"<sup>" in stran):
+                naziv = " ".join(c[1:3])
+                if not (naziv+"<sup>" in stran) or naziv in tujci:
                     while k < 5:
                         stri += c[k]+";"
                         k += 1
