c3b3f3ac789caa4e68d1e5ae97aad88e1c12bbc9
Marko Dudić <marko.dudic@gmail.com>
2023-11-03 18:25:14 +0100

    zaključek 2023

--- sol.py
+++ sol.py
@@ -57,7 +57,7 @@ for st_lige in range(1,12):
         vCsv(stanjeLige,st_tekem,kat,tek)
 
 if path.isfile('./Resna stanja/SOL'+str(st_tekem)+'.csv'):
-    g=open('sol_2022.csv','w',encoding='utf-8')
+    g=open('sol_2024.csv','w',encoding='utf-8')
     with open('./Resna stanja/SOL'+str(st_tekem)+'.csv','r',encoding='utf-8') as f:
         for i in f.readlines():
             g.write(i)
